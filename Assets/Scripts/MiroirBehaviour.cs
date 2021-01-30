fc4ce796fd00185556490699f3e14a0b414bd250
PrivateOh <33682871+PrivateOh@users.noreply.github.com>
2021-01-30 18:50:53 +0100

    Lumières
    Spots, points et miroirs

---
+++ Assets/Scripts/MiroirBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class MiroirBehaviour : MonoBehaviour
+{
+    public int compteurRays = 0;
+    [Range (1,10)]
+    public int nombreRequisRays = 3;
+
+    public bool miroirState = false;
+
+
+    void LateUpdate()
+    {
+        if (compteurRays < nombreRequisRays)
+        {
+            GetComponent<Light2D>().enabled = false;
+            GetComponent<PolygonCollider2D>().enabled = false;
+        }
+
+        compteurRays = 0;
+        miroirState = false;
+    }
+
+    void Awake()
+    {
+        GetComponent<Light2D>().enabled = false;
+        GetComponent<PolygonCollider2D>().enabled = false;
+    }
+
+    public void HitByRay ()
+    {
+        compteurRays++;
+        if (compteurRays >= nombreRequisRays)
+        {
+            miroirState = true;
+            GetComponent<Light2D>().enabled = true;
+            GetComponent<PolygonCollider2D>().enabled = true;
+        }
+        else
+        {
+            GetComponent<Light2D>().enabled = false;
+            GetComponent<PolygonCollider2D>().enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+
+    public float RotationSpeed = 1;
+
+
+    private Quaternion _lookRotation;
+    private Vector3 _direction;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0;
+
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), mousePosition);
+
+            if (hit.collider != null && hit.collider.gameObject.tag == "Rotative")
+            {
+                Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - hit.collider.gameObject.transform.position;
+                diff.Normalize();
+
+                float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+                hit.collider.gameObject.transform.rotation = Quaternion.Euler(0f, 0f, (rot_z - 90));
+            }
+
+
+        }
+    }
+}
