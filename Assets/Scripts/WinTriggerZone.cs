386730eab1a4fea92334634f0b6436facfc43afa
PrivateOh <33682871+PrivateOh@users.noreply.github.com>
2021-01-31 16:10:25 +0100

    Niveau 1 + prefab

---
+++ Assets/Scripts/WinTriggerZone.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinTriggerZone : MonoBehaviour
+{
+    public int numSceneToLoad = 0;
+    public AudioSource winSound;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (winSound != null)
+            {
+                winSound.Play();
+                while (winSound.isPlaying) ;
+            }
+            SceneManager.LoadScene(numSceneToLoad);
+        }
+    }
+}
